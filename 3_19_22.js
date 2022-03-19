04e08a194de25b5248c4100b367c2f7a614e4d99
cybersword112 <pembertoncy@gmail.com>
2022-03-19 09:19:14 -0600

    daily codewar

---
+++ 3_19_22.js
@@ -0,0 +1,23 @@
+function litres(time) {
+  let litres = .5 * time
+  litres = Math.floor(litres)
+  return litres;
+}
+
+function roundTo(n, digits) {
+    let negative = false;
+    if (digits === undefined) {
+        digits = 0;
+    }
+    if (n < 0) {
+        negative = true;
+        n = n * -1;
+    }
+    let multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    n = (Math.round(n) / multiplicator).toFixed(digits);
+    if (negative) {
+        n = (n * -1).toFixed(digits);
+    }
+    return n;
+}
