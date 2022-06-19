d573901565c2afc64a26631c0a98aa2dd7e198e6
cybersword112 <pembertoncy@gmail.com>
2022-06-19 08:26:51 -0500

    daily codewar

---
+++ 6_19_22.js
@@ -0,0 +1,12 @@
+function divisors(integer) {
+    let res =[]
+    for(let i = 2; i<integer; i++){
+      if(integer%i == 0){
+        res.push(i)
+      }
+    }
+    if(res.length == 0){
+      return `${integer} is prime`
+    }
+    return res
+  };
