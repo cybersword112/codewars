4db3e9a23a2325d9d6d85bfb492decab16450069
cybersword112 <pembertoncy@gmail.com>
2022-07-22 17:57:41 -0500

    daily codewar

---
+++ 7_22_22.js
@@ -0,0 +1,5 @@
+const areaOrPerimeter = function(l , w) {
+    //   check if sides are equal, if so return square, if not ,return sum
+      return l === w ? l**2 : (l*2) + (w*2)
+      // Return your answer
+    };
