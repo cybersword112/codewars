1693ac34e05a987aef84d787260e9f78d49132a4
cybersword112 <pembertoncy@gmail.com>
2022-08-30 11:15:31 -0700

    array code review

---
+++ 8_30_22.js
@@ -0,0 +1,10 @@
+function sortByLength (array) {
+    // Return an array containing the same strings, ordered from shortest to longest
+    return array.sort((a,b)=>{return a.length - b.length})
+  // sort the array, to sort compare length of a and b
+  //   return the array
+  };
+  
+  
+  // will be provided an array of strings
+  // output should be the same array with strings sorted by length
