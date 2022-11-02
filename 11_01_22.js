5de39b01aec3edd2592c03734ad13bf78e6aeada
cybersword112 <pembertoncy@gmail.com>
2022-11-01 19:12:05 -0700

    daily code challenge

---
+++ 11_01_22.js
@@ -0,0 +1,7 @@
+function rowSumOddNumbers(n) {
+    //   this is purely a test of math knowledge, it would be alittle crazy to solve by most manual methods
+      return Math.pow(n, 3);
+    }
+    
+    // for a pyramid of consecutive odd numbers (1 being the point, 3, 5 being base level two etc)
+    //  return the sum of the nth level of this pyramid
